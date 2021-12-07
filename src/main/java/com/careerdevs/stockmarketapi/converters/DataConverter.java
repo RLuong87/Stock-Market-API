6d916580cef79ca593bb60bbf66bbbca6e067fb5
Richies Luong <richiesluong.student@careerdevs.com>
2021-12-07 17:55:29 -0500

    Data converter class to get specific keys from our Stock Data

--- src/main/java/com/careerdevs/stockmarketapi/converters/DataConverter.java
+++ src/main/java/com/careerdevs/stockmarketapi/converters/DataConverter.java
@@ -1,2 +1,57 @@
-package com.careerdevs.stockmarketapi.converters;public class DataConverter {
+package com.careerdevs.stockmarketapi.converters;
+
+import com.careerdevs.stockmarketapi.models.CompAV;
+import com.careerdevs.stockmarketapi.models.CompCSV;
+
+public class DataConverter {
+
+
+    public static CompCSV convertToFeature1(CompCSV comp) {
+        CompCSV tempComp = new CompCSV();
+
+        tempComp.setName(comp.getName());
+        tempComp.setSymbol(comp.getSymbol());
+        tempComp.setExchange(comp.getExchange());
+
+        return tempComp;
+    }
+
+
+    public static CompAV feature5(String symbol, String assetType, String name, String description, String address) {
+
+        CompAV newComp = new CompAV();
+
+        newComp.setSymbol(symbol);
+        newComp.setAssetType(assetType);
+        newComp.setName(name);
+        newComp.setDescription(description);
+        newComp.setAddress(address);
+
+        return newComp;
+    }
+
+    public static CompAV convertToFeature5(CompAV comp) {
+
+        CompAV newComp = new CompAV();
+
+        newComp.setSymbol(comp.getSymbol());
+        newComp.setAssetType(comp.getAssetType());
+        newComp.setName(comp.getName());
+        newComp.setDescription(comp.getDescription());
+        newComp.setAddress(comp.getAddress());
+
+        return newComp;
+    }
+
+    public static CompAV convertToFeature6(CompAV comp) {
+
+        CompAV newComp = new CompAV();
+
+        newComp.setSymbol(comp.getSymbol());
+        newComp.setMarketCapitalization(comp.getMarketCapitalization());
+        newComp.setName(comp.getName());
+
+        return newComp;
+    }
+
 }
